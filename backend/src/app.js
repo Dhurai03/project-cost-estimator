9b133f5f13f3cb0032c1c506062d17979f5eea73
Dhurai03 <149706515+Dhurai03@users.noreply.github.com>
2026-04-07 14:59:07 +0530

    final

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,13 @@ app.use(helmet({
   noCache: true // Enable no-cache headers
 }));
 
-const cors = require('cors');
+// app.use(cors({
+//   origin: process.env.NODE_ENV === 'production'
+//     ? process.env.FRONTEND_URL
+//     : ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+//   credentials: true
+// }));
+
 
 app.use(cors({
   origin: [
